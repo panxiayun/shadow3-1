a01374f8c828666f6dfa548e2350584513bc6c87
robnagler <github@q33.us>
2015-05-22 21:29:22 +0000

    added author,url b/c required; need gfortran in library list

--- setup.py
+++ setup.py
@@ -10,16 +10,18 @@ headers = glob (os.path.join ("Include","*.h") )
 
 setup ( name = "Shadow",
         version = "0.1.0",
+        author_email = 'srio@esrf.eu',
+        author = 'Franco Cerrina, Chris Welnak, G.J. Chen and M. Sanchez del Rio',
+        url = 'http://ftp.esrf.eu/pub/scisoft/shadow3/',
 	packages=["Shadow"],
         package_dir={"Shadow":"."},
         ext_modules = [Extension('Shadow.ShadowLib',
                                  ['shadow_bind_python.c'],
                                  include_dirs  = ['.', numpy.get_include()],
                                  library_dirs  = ['.'],
-                                 libraries     = ['shadow3','shadow3c'],
+                                 libraries     = ['shadow3','shadow3c', 'gfortran'],
                                  extra_compile_args = ['-msse','-msse2'],
                                  extra_link_args = ['-msse','-msse2']
                                 ),
                       ]
         )
-
